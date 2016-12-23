bb705736f0d0715cc99d96412b41de83b47855a2
djbedford <david.bedford@netsend.com>
2016-12-23 13:54:16 +0000

    Implement a controller to handle static pages

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function getIndex()
+    {
+        return view('welcome');
+    }
+
+    public function getAbout()
+    {
+        return view('about');
+    }
+
+    public function getContact()
+    {
+        return view('contact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,8 @@
 |
 */
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('contact', 'PagesController@getContact');
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('about', 'PagesController@getAbout');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@getIndex');
