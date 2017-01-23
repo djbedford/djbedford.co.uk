58c2df74a00bea0d33b0569ba4e8aa7676ecd172
David Bedford <beds2004@hotmail.com>
2017-01-23 20:11:17 +0000

    Set up the app to allow for the handling of categories

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,24 @@
 |
 */
 
+// Authentication Routes
 Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
 Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
+
+// Registration Routes
 Route::get('register', 'Auth\RegisterController@showRegistrationForm');
 Route::post('register', 'Auth\RegisterController@register');
+
+// Password Reset Routes
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::get('password/reset/{token?}', 'Auth\ResetPasswordController@showResetForm');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+// Categories
+Route::resource('categories', 'CategoryController', ['except' => ['create']]);
+
 Route::get('blog/{slug}', ['as' => 'blog.single', 'uses' => 'BlogController@getSingle'])
     ->where('slug', '[\w\d\-\_]+');
 Route::get('blog', ['as' => 'blog.index', 'uses' => 'BlogController@getIndex']);
