623cdc57cce45b0ed5fb46c9f6d24bb18c1898f2
David Bedford <beds2004@hotmail.com>
2017-01-23 20:01:23 +0000

    Finish the authentication system by adding a reset password feature

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,6 +14,7 @@
                 {{ Form::checkbox('remember') }}{{ Form::label('remember', 'Remember Me') }}
                 <br>
                 {{ Form::submit('Login', ['class' => 'btn btn-primary']) }}
+                <p><a href="{{ url('password/reset') }}">Forgot My Password</a></p>
             {!! Form::close() !!}
         </div>
     </div>
